4c38bd85438012e4f5916bb9a403c03fc219dd0a
Gabriel Carvalheiro Ruela <141842543+GabrielCarvalheiroRuela@users.noreply.github.com>
2024-10-25 13:03:24 -0300

    feature: integração firebase authentication

--- app/src/main/java/com/example/cardapio_pizzaria/LoginActivity.kt
+++ app/src/main/java/com/example/cardapio_pizzaria/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cardapio_pizzaria.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
